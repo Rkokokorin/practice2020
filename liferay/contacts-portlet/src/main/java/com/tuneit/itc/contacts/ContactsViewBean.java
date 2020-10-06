9f936e59726430440e28aacd4905de5f41aca3ad
Arsenii <ar.poryadin@yandex.ru>
2020-10-06 17:15:50 +0300

    resizable map & start of add city functional impl

--- liferay/contacts-portlet/src/main/java/com/tuneit/itc/contacts/ContactsViewBean.java
+++ liferay/contacts-portlet/src/main/java/com/tuneit/itc/contacts/ContactsViewBean.java
@@ -1,5 +1,6 @@
 package com.tuneit.itc.contacts;
 
+import com.liferay.faces.portal.el.internal.Liferay;
 import com.tuneit.itc.commons.model.OfficeContactInfo;
 import com.tuneit.itc.commons.service.OfficeContactInfoService;
 import lombok.Data;
@@ -13,13 +14,51 @@ import javax.faces.bean.ViewScoped;
 @ViewScoped
 @ManagedBean
 public class ContactsViewBean {
+    private static final String DEFAULT_CITY = "Санкт-Петербург";
+    private String shownCity;
     private OfficeContactInfo contactInfo;
 
     @ManagedProperty("#{officeContactInfoService}")
     private OfficeContactInfoService officeContactInfoService;
+    @ManagedProperty("#{liferay}")
+    private Liferay liferay;
+
+    private Mode mode;
 
     @PostConstruct
     public void init() {
-        contactInfo = officeContactInfoService.findByCity("Санкт-Петербург");
+        updateShownCity(DEFAULT_CITY);
+    }
+
+    public void updateShownCity(String city) {
+        shownCity = city;
+        contactInfo = officeContactInfoService.findByCity(shownCity);
+        //update view too
+        mode = Mode.VIEW;
+    }
+
+    public boolean isAdmin() {
+        return liferay.getThemeDisplay().getPermissionChecker().isOmniadmin();
+    }
+
+    public boolean isModeEdit() {
+        return mode == Mode.EDIT;
+    }
+
+    public boolean isModeView() {
+        return mode == Mode.VIEW;
+    }
+
+    public void setModeEdit() {
+        mode = Mode.EDIT;
+    }
+
+    public void setModeView() {
+        mode = Mode.VIEW;
+    }
+
+    private enum Mode {
+        VIEW,
+        EDIT
     }
 }
